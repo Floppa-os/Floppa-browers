4a6ab21fc8d5ca9a4b88ef5e1fbef3560873fefe
Mickredset <aivliev782@gmail.com>
2026-01-29 21:59:41 +0300

    Добавил сообщения в консоль

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import sys
 from PyQt6.QtWidgets import QApplication
 from browser_window import MainWindow
-
+print("import QWebEngineView")
+print("import sys")
+print("from PyQt6.QtWidgets import QApplication")
 def main():
     app = QApplication(sys.argv)
     window = MainWindow()
